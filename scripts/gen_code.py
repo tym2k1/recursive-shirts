22cac49d1b76d4748743b6d90716e870fa578bcb
Tymoteusz Burak <tymbur@gmail.com>
2026-02-18 21:40:42 +0100

    feat: simplify brainfuck shirt logic

--- scripts/gen_code.py
+++ scripts/gen_code.py
@@ -14,12 +14,8 @@ def parse_args():
                         help="Font family (default: monospace)")
     parser.add_argument("--font-size", type=float, default=3,
                         help="Font size (default: 3)")
-    parser.add_argument("--width", type=int, default=63,
-                        help="SVG width (default: 63)")
-    parser.add_argument("--height", type=int, default=60,
-                        help="SVG height (default: 60)")
-    parser.add_argument("--y-increment", type=float, default=5,
-                        help="Y increment per line (default: 5)")
+    parser.add_argument("--line-height", type=float, default=3,
+                        help="Y increment per line (default: 3)")
     parser.add_argument("--initial-y", type=float, default=3,
                         help="Initial Y position (default: 3)")
 
@@ -29,9 +25,8 @@ def parse_args():
 def svg_header(args):
     return (
         f'<svg font-family="{args.font}" '
-        f'width="{args.width}" '
-        f'height="{args.height}" '
-        f'font-size="{args.font_size}">'
+        f'font-size="{args.font_size}" '
+        f'style="white-space:pre;line-height:{args.line_height}">'
     )
 
 
@@ -41,58 +36,39 @@ def main():
     final_code = ""
 
     # SVG Header
-    header = svg_header(args)
-    header += '<text x="0" y="'
-    header += str(args.initial_y)
-    header += '">'
-    header_code, _, _, header_cells_state, header_ptr = autotune(header)
+    print_code = svg_header(args)
+    print_code += '<text y="'
+    print_code += str(args.initial_y)
+    print_code += '">'
+    print_code += '<![CDATA['
 
-    # Dont use cells used for printing header later in the code (potential optimization area)
-    move_right = len(header_cells_state) - header_ptr
-    pointer_alignment = ">" * move_right if move_right > 0 else ""
+    char_count_header = len(print_code)
 
-    y_increment_setup = ">" + "+" * args.initial_y + "<"
+    print_code += ']]></text></svg>'
+    print_code, _, _, print_cells_state, print_ptr = autotune(print_code)
 
-    # initial part of logic for "print every character until new line
-    detect_newline_logic = ",[----------[[++++++++++.[-]],----------]"
+    printing_logic = ",[.,]"
 
-    # result of `autotune` for '</text><text x="0" y="'. Adjusted to clean cells
-    # after itself as it'll run arbitrary amount of times and not touch the increment cell
-    newline_prefix_1 = ">>+++++++++++++++[>++++>+++>++++++++>+++++++>++++++++>++++>++<<<<<<<-]>.>++.>----.>----.>.<<.>>>++.--.<<<.>.>.<<.>>>>++.<<.>+.>++.<<<<<+.>>>>>.--.<<+.>.>++.[-]<[-]<[-]<[-]<[-]<[-]<[-]<<<>"
+    move_right = len(print_cells_state) - print_ptr
+    printing_pointer_alignment = ">" * move_right + printing_logic + "<" * move_right if move_right > 0 else printing_logic
 
-    # Taken from https://esolangs.org/wiki/Brainfuck_algorithms#Print_value_of_cell_x_as_number_for_ANY_sized_cell_(eg_8bit,_100000bit_etc
-    print_increment_value_logic = ">[-]>[-]+>[-]+<[>[-<-<<[->+>+<<]>[-<+>]>>]++++++++++>[-]+>[-]>[-]>[-]<<<<<[->-[>+>>]>[[-<+>]+>+>>]<<<<<]>>-[-<<+>>]<[-]++++++++[-<++++++>]>>[-<<+>>]<<]<[.[-]<]<<"
+    # Insert printing_pointer_alignment after char_count_header dots
+    dot_count = 0
+    inserted = False
+    new_print_code = []
 
-    # print '">'
-    newline_prefix_2 = ">>+++++++[>+++++>+++++++++<<-]>-.>-.[-]<[-]<<<"
 
-    # quits when end of file
-    detect_newline_end_logic = ",]"
+    for c in print_code:
+        new_print_code.append(c)
 
-    exit_svg = ">>+++++++++++++++[>++++>+++>++++++++>+++++++>++++<<<<<-]>.>++.>----.>----.<++++.----.>>++.--.<<<.>-.+++.>++.>++."
+        if c == '.':
+            dot_count += 1
+            if dot_count == char_count_header and not inserted:
+                new_print_code.append(printing_pointer_alignment)
+                inserted = True
 
-    y_increment_logic = "+" * args.y_increment
-
-    sections = [
-        header_code,
-        pointer_alignment,
-        y_increment_setup,
-        detect_newline_logic,
-        newline_prefix_1,
-        y_increment_logic,
-        print_increment_value_logic,
-        newline_prefix_2,
-        detect_newline_end_logic,
-        exit_svg,
-    ]
-
-    final_code = "".join(sections)
-
-    print("Final code:")
+    final_code = ''.join(new_print_code)
     print(final_code)
-    print()
-    print("Code length:")
-    print(len(final_code))
 
 if __name__ == "__main__":
     main()
